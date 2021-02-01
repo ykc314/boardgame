910d749dc8f846b09430da6cc4a321ad3ef74667
ykc314 <ykc314@gmail.com>
2021-02-02 00:03:33 +0900

    フォルダ構成変更

---
+++ boardgame/osero/js/script.js
@@ -0,0 +1,17 @@
+let banArr = new Array(8);
+for (let i = 0; 8 > banArr.length; i++) {
+  banArr[i] = new Array(8);
+}
+
+// HTMLで定義したテーブルを取得
+let ban = document.getElementById('ban');
+
+// 取得したテーブルに盤面生成
+banNew();
+
+// 盤面を初期化する
+banInit();
+
+// クリックした時に実行されるイベント
+for (let )
+
